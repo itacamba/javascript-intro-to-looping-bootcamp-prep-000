bcd02ac226f118b3b9e30837acd12183a5318378
brave-zombie-1932 <daniela.sahagun@hotmail.com>
2020-01-17 18:52:17 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -21,9 +21,9 @@ function doWhileLoop(num){
   function incrementVariable(){
     i = i + 1;
     return i;
+  }
     do {
       console.log('I run once regardless.');
     } while(incrementVariable() < num);
-  }
 
 }
