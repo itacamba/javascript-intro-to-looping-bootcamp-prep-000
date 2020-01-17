b32f5129ba55b8f890a250d98a6c372cd62a4301
brave-zombie-1932 <daniela.sahagun@hotmail.com>
2020-01-17 18:35:25 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ function forLoop(array){
     if(i === 1){
       [...array,`I am 1 strange loop.`];
     } else{
-      [array,`I am ${i} strange loops.`];
+      [...array,`I am ${i} strange loops.`];
     }
   }
 }
