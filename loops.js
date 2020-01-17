85ae04b17fe6faa75c6c47acbd805af0b4f29335
brave-zombie-1932 <daniela.sahagun@hotmail.com>
2020-01-17 19:03:27 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,11 +1,12 @@
 function forLoop(array){
   for(let i = 0; i < 25 ; i++){
     if(i === 1){
-      return array + `I am 1 strange loop.`;
+      array + `I am 1 strange loop.`;
     } else{
-      return array + `I am ${i} strange loops.`;
+      array + `I am ${i} strange loops.`;
     }
   }
+  return array;
 }
 
 
