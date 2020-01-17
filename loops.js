2ca640d7d765c9a7ee383c8f9511286a927413cf
brave-zombie-1932 <daniela.sahagun@hotmail.com>
2020-01-17 19:29:12 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,13 +1,18 @@
-// function forLoop(array){
-//   for(let i = 0; i < 25 ; i++){
-//       if(i === 1){
-//           console.log(array + `I am 1 strange Loop`);
-//         } else {
-//           console.log(array + `I am ${i} strange loops.`);
-//         }
-//   }
-// }
-
+function forLoop(array){
+  for(let i = 0; i < 25 ; i++){
+      if(i === 1){
+          console.log(array + `I am 1 strange Loop`);
+        } else {
+          console.log(array + `I am ${i} strange loops.`);
+        }
+  }
+}
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+  array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`);
+   }
+    return array;
+ }
 
 function whileLoop(number){
   while(number > 0){
@@ -27,10 +32,3 @@ function doWhileLoop(num){
     } while(incrementVariable() < num);
 
 }
-
-function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-  array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`);
-   }
-    return array;
- }
